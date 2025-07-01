86f5a20fee65461e461ba42d031a0ed969f8245d
Hnatovskyi Davyd <mailforgamestm@gmail.com>
2025-07-01 16:11:11 +0300

    added homework №5

---
+++ homework-05/SafeBank.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <random>
+#include <mutex>
+#include <atomic>
+
+class SafeBankAccount
+{
+private:
+    double balance = 1000.0;
+    mutable std::mutex balance_mutex;
+
+public:
+    void deposit(double amount)
+    {
+        std::lock_guard<std::mutex> lock(balance_mutex);
+        balance+=amount;
+    }
+
+    bool withdraw(double amount)
+    {   
+        if (balance>=amount)
+        {
+            std::lock_guard<std::mutex> lock(balance_mutex);
+            balance-=amount;
+            return true;
+        }
+        return false;
+
+    }
+
+    double get_balance() const
+    {
+        std::lock_guard<std::mutex> lock(balance_mutex);
+        return balance;
+    }
+};
+
+class SafeBankStatistics
+{
+private:
+    std::atomic<int> transaction_count{0};
+    std::atomic<double> total_amount{0.0};
+
+public:
+    void record_transaction(double amount)
+    {
+        transaction_count++;
+        
+        double current = total_amount.load();
+        double desired;
+        do {
+            desired = current + amount;
+        } while (!total_amount.compare_exchange_weak(current, desired));
+    }
+
+    int get_total_transactions() const
+    {
+        return transaction_count.load();
+    }
+
+    double get_total_amount() const
+    {
+        return total_amount.load();
+    }
+};
+
+void cashierWork(SafeBankAccount &bankAccount, SafeBankStatistics &statist)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> deposit(50, 500);
+    std::uniform_int_distribution<> withdraw(10, 200);
+    std::bernoulli_distribution operation(0.5);
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (operation(gen))
+        {
+            double amount = deposit(gen);
+            bankAccount.deposit(amount);
+            statist.record_transaction(amount);
+        }
+        else
+        {
+            double amount = withdraw(gen);
+            if (bankAccount.withdraw(amount))
+                statist.record_transaction(amount);
+        }
+    }
+}
+
+int main()
+{
+    SafeBankAccount safeAccount;
+    SafeBankStatistics statist;
+    std::vector<std::thread> threads(5);
+    for (int i = 0; i < 5; i++)
+    {
+        threads.emplace_back(cashierWork, std::ref(safeAccount), std::ref(statist));
+    }
+
+    for (auto &thread : threads)
+    {
+        if (thread.joinable())
+        {
+            thread.join();
+        }
+    }
+
+    std::cout << "================RESULT================\n";
+    std::cout << "Total transactions: " << statist.get_total_transactions() << std::endl;
+    std::cout << "Total transactions amount: " << statist.get_total_amount() << std::endl;
+    std::cout << "Final balance: " << safeAccount.get_balance() << std::endl;
+}
